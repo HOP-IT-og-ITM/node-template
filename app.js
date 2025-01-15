bf51ac858f16d74263a4cd1ea7e55bc61c9a4605
Egil Johnsen Dahle <egil_dahle@hotmail.com>
2025-01-15 12:42:18 +0100

    Simplified the code

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const { WebSocketServer } = require('ws');
 const fs = require('fs');
+
 const app = express();
 const port = 3000;
 const server = http.createServer(app);
 
 
-// Opprett WebSocket-server
-const wss = new WebSocketServer({ server });
-wss.on('connection', (ws) => {
-    console.log('WebSocket connection established');
-});
-
-
-// Send live-reload-melding til alle klienter
-const broadcastReload = () => {
-    wss.clients.forEach((client) => {
-        if (client.readyState === 1) { // WebSocket er åpen
-            client.send('reload');
-        }
-    });
-};
-
-
-// Overvåk filendringer i `public`-mappen
-fs.watch(path.join(__dirname, 'public'), { recursive: true }, (eventType, filename) => {
-    console.log(`File changed: ${filename}`);
-    broadcastReload();
-});
-
 
 // Setter public som root-mappe
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routing til ulike filer, legg inn egne ved å kopiere en av app.get-funkjonene
 app.get('/', (req, res) => {
@@ -44,8 +21,6 @@ app.get('/', (req, res) => {
 
 
 
-
-// Start HTTP- og WebSocket-server
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
